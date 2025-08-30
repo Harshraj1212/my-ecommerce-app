114d0d13372d1d9531902483d255f0e822a456ad
Harsh Rajput <harsh.rajput@maiginfotech.com>
2025-08-30 12:39:35 +0530

    Initial commit

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,11 +1,15 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config(); // load env variables
 
 let pool = mysql.createPool({
-    connectionLimit: 100,
-    user: "root",
-    password: "Harsh@123",
-    database: "shoesmall",
-    host: "localhost"
+  connectionLimit: 100,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+ port: process.env.MYSQL_PORT
 });
 
 export default pool;
